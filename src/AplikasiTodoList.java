c61f5c063b4278c5a9ba697f5d54a7cd92010176
Kevin Williams Permana <kevinity310@gmail.com>
2023-06-22 11:46:41 +0700

    Menentukan Business Logic

---
+++ src/AplikasiTodoList.java
@@ -0,0 +1,31 @@
+public class AplikasiTodoList {
+
+    public static String[] model = new String[];
+
+    public static void main(String[] args){
+
+
+    }
+    //    Business Logic
+    /**
+     * Menampilkan todo list
+     */
+    public static void showTodoList(){
+
+    }
+
+    /**
+     * Menambah todo list
+     */
+    public static void addTodoList(){
+
+    }
+
+    /**
+     * Menghapus todo list
+     */
+    public static void removeTodoList(){
+
+    }
+
+}
